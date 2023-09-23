df1edb5e60edf631955453a268b85c4aa8553b87
Misha Panivnyk <panivnykm@gmail.com>
2023-09-23 14:45:05 +0300

    add Header to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
+import React from 'react';
+import { Header } from './Header/Header';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Header />
+      {/* Остальной контент вашего приложения */}
     </div>
   );
-};
+}
