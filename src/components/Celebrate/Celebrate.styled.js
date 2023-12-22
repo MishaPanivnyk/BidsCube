0cdf310ee5b5a213293894a5133bcb4ffe4632c6
Misha Panivnyk <panivnykm@gmail.com>
2023-12-22 18:55:10 +0200

    add component/Celebrate

---
+++ src/components/Celebrate/Celebrate.styled.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+export const CelebrateContainer = styled.section`
+  padding-bottom: 60px;
+  padding-top: 52px;
+`;
+
+export const CelebrateTitle = styled.h2`
+  color: #505050;
+  text-align: center;
+  font-family: 'Stolzl';
+  font-size: 38px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-bottom: 24px;
+`;
+
+export const CelebrateDesk = styled.p`
+  color: #505050;
+  text-align: center;
+  font-family: 'Manrope';
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  max-width: 680px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+`;
+export const CelebrateDeskHashtagContainer = styled.div`
+  display: flex;
+  gap: 45px;
+  justify-content: center;
+`;
+export const CelebrateImgContainer = styled.div`
+  margin-bottom: 40px;
+`;
+export const CelebrateDeskHashtag = styled.p`
+  color: #5070b6;
+  text-align: center;
+  font-family: 'Stolzl';
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-transform: lowercase;
+`;
