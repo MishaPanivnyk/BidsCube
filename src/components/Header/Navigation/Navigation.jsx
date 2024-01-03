c4cc23adc5a21cac8da5267943614a1d19d3012b
Misha Panivnyk <panivnykm@gmail.com>
2024-01-03 20:40:28 +0200

    add mob-style/Navigation

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,19 +1,26 @@
-import { Link, Nav } from './Navigation.styled';
+import { Hamburger, Link, Nav } from './Navigation.styled';
+import hamburger from 'img/hamburger.png';
+
 export const Navigation = ({ onClick }) => {
   return (
-    <Nav>
-      <Link to="/" onClick={onClick}>
-        About event
-      </Link>
-      <Link to="/" onClick={onClick}>
-        Team
-      </Link>
-      <Link to="/" onClick={onClick}>
-        Venue
-      </Link>
-      <Link to="/" onClick={onClick}>
-        Join Us
-      </Link>
-    </Nav>
+    <>
+      {/* <button>
+        <Hamburger src={hamburger} alt="menu" />
+      </button> */}
+      <Nav>
+        <Link to="/" onClick={onClick}>
+          About event
+        </Link>
+        <Link to="/" onClick={onClick}>
+          Team
+        </Link>
+        <Link to="/" onClick={onClick}>
+          Venue
+        </Link>
+        <Link to="/" onClick={onClick}>
+          Join Us
+        </Link>
+      </Nav>
+    </>
   );
 };
--- src/components/Header/Navigation/Navigation.styled.js
+++ src/components/Header/Navigation/Navigation.styled.js
@@ -4,6 +4,9 @@ export const Nav = styled.nav`
   display: flex;
   align-items: center;
   gap: 33px;
+  @media screen and (max-width: 1280px) {
+    display: none;
+  }
 `;
 export const Link = styled.a`
   display: flex;
@@ -16,4 +19,10 @@ export const Link = styled.a`
   font-weight: 400;
   line-height: normal;
   cursor: pointer;
+`;
+export const Hamburger = styled.img`
+  display: none;
+  @media screen and (max-width: 1280px) {
+    display: block;
+  }
 `;
